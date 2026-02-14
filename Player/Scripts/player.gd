b857642f50b5fbddfea3421b1f616d80bef6dfb8
rwbrowne <rwbrowne@gmail.com>
2026-02-14 19:29:40 +0000

    Player physics control option

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 
 @export var speed : float = 200
 @export var animation_tree : AnimationTree
+@export var physicscontrol : bool = false
+
+const MAX_SPEED = 300.0
+const ACCELERATION = 800.0
+const FRICTION = 900.0
+
 
 var input :Vector2
 var playback : AnimationNodeStateMachinePlayback
@@ -13,7 +19,18 @@ func _ready():
 
 func _process(delta: float) -> void:
 	input = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input * speed	
+	
+	# Check player control to use physics based control
+	if physicscontrol:
+		if input:
+			# Acceleration (Weighty start)
+			velocity = velocity.move_toward(input * MAX_SPEED, ACCELERATION * delta)
+		else:
+			# Friction (Weighty stop)
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	else:
+		velocity = input * speed
+	
 	move_and_slide()
 	select_animation()
 	update_animation_parameters()
@@ -21,6 +38,8 @@ func _process(delta: float) -> void:
 func select_animation():
 	if velocity == Vector2.ZERO:
 		playback.travel("Idle")
+	elif velocity.length() < 150:
+		playback.travel("Walk")
 	else:
 		playback.travel("Run")
 
